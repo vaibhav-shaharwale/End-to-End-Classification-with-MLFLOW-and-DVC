6e3f07d39def790f6465159fe05638aa4d508639
Vaibhav Shaharwale <109690434+vaibhav-shaharwale@users.noreply.github.com>
2024-02-06 11:58:50 +0530

    updated

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -9,7 +9,7 @@ class PredictionPipeline:
 
 
     def predict(self):
-        model = load_model(od.path.join("artifacts", "training", "model.h5"))
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
 
         image_name = self.filename
         test_image = image.load_img(image_name, target_size = (224,224))
@@ -20,9 +20,9 @@ class PredictionPipeline:
 
 
         if result[0]==1:
-            prediction = "Normal"
+            prediction = "Tumor"
             return [{"image" : prediction}]
 
         else:
-            prediction = "Tumor"
+            prediction = "Normal"
             return [{"image" : prediction}]
